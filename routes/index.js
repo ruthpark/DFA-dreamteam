64abe6a93e506c93225d7b1d79e0ae6cfeb70d65
Jordan Wick <Jordan Wick>
2016-03-31 22:07:12 -0400

    Create a Friends Page

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,13 @@ router.get('/profile', function (req, res, next) {
   res.render('profile');
 
 });
+router.get('/friends', function (req, res, next) {
+
+  // Rendering the index view with the title 'Sign Up'
+  res.render('friends');
+  
+});
+
 
 /* GET userlist JSON */
 router.get('/userlist', function (req, res, next) {
